2672088ebdcefa1cd877c2a346463ec5cde2f307
vsyrgkanis <bsyrganis@gmail.com>
2024-09-12 07:34:53 -0700

    Added better semi-synthetic generation
    So that now coverage is correct. Prior there was a weirdness in drawing from raw pairs of X, Z.

--- proximalde/utilities.py
+++ proximalde/utilities.py
@@ -1,4 +1,6 @@
 import numpy as np
+import warnings
+import scipy.linalg
 
 
 def _check_input(*args):
@@ -11,3 +13,64 @@ def _check_input(*args):
                              'of samples')
     # reshape all variables to be 2d matrices
     return (arg.reshape(-1, 1) if (arg is not None and len(arg.shape) == 1) else arg for arg in args)
+
+
+def covariance(X, Z):
+    ''' Calculate covariance matrix of two random vectors.
+    Each row of X, Z contains a sample of the two vectors.
+    '''
+    assert X.shape[0] == Z.shape[0], "First dimension must match!"
+    return (X - X.mean(axis=0, keepdims=True)).T @ (Z - Z.mean(axis=0, keepdims=True)) / X.shape[0]
+
+
+def svd_critical_value(Z, X, *, alpha=0.05, mc_samples=1000):
+    ''' Calculates critical value on the singular values of a covariance
+    matrix of a pair of random vectors, above which they can be deemed
+    as non-zero statistically.
+
+    Parameters
+    ----------
+    Z : ArrayLike[n, pz]
+    X : ArrayLike[n, px]
+    alpha : float, optional (default=0.05)
+        confidence level
+    mc_samples : int, optional (default=1000)
+        monte carlo samples use to estimate critical value
+
+    Returns
+    -------
+    critical_value : float
+        The critical value for a statistically non-zero singular value of
+        the covariance Cov(Z, X)
+    '''
+    # the line below creates a matrix of (n, px * pz), which could be quite large
+    # we need this matrix to calculate the "covariance" of the entries of the covariance
+    # matrix En[X Z'], whose eigenvalues we then want to compute.
+    n, pz = Z.shape
+    px = X.shape[1]
+
+    # if data too large, we do a monte carlo approximation of the eigenvalues
+    # though in the end we divide these eigenvalues appropriately by the original
+    # size n
+    if n * px * pz > 1e9:
+        subset = np.random.choice(n, size=int(1e9 // (px * pz)), replace=False)
+        Z, X = Z[subset], X[subset]
+        warnings.warn("Due to large sample size and proxy dimension, we performed "
+                        "monte-carlo approximation of critical value using "
+                        f"random subset of n={len(subset)} samples and then re-scaled "
+                        "appropriately to account for the larger original sample size.")
+
+    cZX = (Z.reshape(Z.shape + (1,)) * X.reshape(X.shape + (1,)).transpose((0, 2, 1))).reshape(Z.shape[0], -1)
+    cZX = cZX - cZX.mean(axis=0, keepdims=True)
+    cZXcov = cZX.T @ cZX / cZX.shape[0]
+
+    # here we compute eigenvalues of a symmetric (px * pz, px * pz) matrix
+    # this can also be a bit slow. We scale by the original size n, not the
+    # potentially subsampled size.
+    eigs = scipy.linalg.eigvalsh(cZXcov / n)
+
+    # calculate the critical value via monte carlo simulation of the percentile
+    # of the weighted sum of independent chi-square distributed variables
+    samples = np.random.chisquare(df=1, size=(mc_samples, len(eigs))) @ eigs
+    critical = np.sqrt(np.percentile(samples, (1 - alpha) * 100))
+    return critical
